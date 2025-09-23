a88ca5bab10193add0d8754fb020cd7b5c60abd2
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-09-23 11:14:07 +0530

    Add implementation of undirected and weighted graph using adjacency matrix

---
+++ Graph/Graph_Representations/Adjacency_Matrix/02.Undirected_and_Weighted_graph.cpp
@@ -0,0 +1,32 @@
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int vertice,edge ;
+    vertice = 5;
+    edge = 6;
+    
+    vector<vector<int>> adj(vertice , vector<int>(vertice , 0));
+    
+    int u,v,value;
+    u=2;
+    v=1;
+    value = 3;
+    
+    // undirected and weighted 
+    for(int i = 0 ; i<edge ; i++){
+        adj[u][v] = value;
+        adj[v][u] = value;
+    }
+    
+    for(int i =0; i<vertice ; i++){
+        for(int a = 0 ; a<vertice ; a++){
+            cout<< adj[i][a] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
